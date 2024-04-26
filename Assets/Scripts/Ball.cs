74c843e16224c2973df1a67b4d2424c2da49607f
viZkaHD <pavi119@vidalibarraquer.net>
2024-04-26 10:47:13 +0200

    controller

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.XR.PXR;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private bool isHeld = false;
+    private Transform controllerTransform; // Se guarda la referencia al transform del controlador
+
+    // Inicialización
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous; // Mejor para objetos de alta velocidad.
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Si la pelota está siendo sostenida, se actualiza su posición para que siga al controlador
+        if (isHeld && controllerTransform != null)
+        {
+            transform.position = controllerTransform.position;
+        }
+    }
+
+    public void GrabBall(Transform controller)
+    {
+        // Llamar cuando el objeto es agarrado por el controlador derecho.
+        isHeld = true;
+        rb.isKinematic = true;
+        controllerTransform = controller; // Guarda la referencia al controlador
+        transform.SetParent(controller);
+        transform.localPosition = Vector3.zero;
+    }
+
+    public void ReleaseBall(Vector3 throwForce)
+    {
+        // Llamar cuando el objeto es soltado por el controlador derecho.
+        transform.SetParent(null);
+        isHeld = false;
+        rb.isKinematic = false;
+        rb.velocity = throwForce; // Aquí puedes usar rb.velocity para mantener la velocidad del controlador al soltar
     }
 }
